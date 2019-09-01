e729768a1d899f6a85bc0746c27abb88ac706546
marco-yc-song <marco@peopet.co.kr>
2019-09-01 12:02:37 +0900

    8.6 TS에서 이벤트 핸들링하는 법@@

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Number from "./Number";
+import { Form, Input } from "./Input";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IState {
+  counter: number;
+  name: string;
+}
+
+// <props, state, ~~~>로 ts에게 state가 뭘로 구성된지 알려줌
+class App extends Component<{}, IState> {
+  state = {
+    counter: 0,
+    name: ""
+  };
+
+  render() {
+    const { counter, name } = this.state;
+
+    return (
+      <div>
+        <Form onFormSubmit={this.onFormSubmit}>
+          <Input value={name} onChange={this.onChange} />
+        </Form>
+
+        <Number count={counter} />
+        <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
+
+  onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    console.log(event.target);
+  };
+
+  onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
+  add = () => {
+    this.setState(prevState => {
+      return {
+        counter: prevState.counter + 1
+      };
+    });
+  };
 }
 
 export default App;
